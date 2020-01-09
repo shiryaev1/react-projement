5dd1ba5b10d949b54996a0c86253aef2d6ac88c5
Nikita <shiryaevv1@yandex.by>
2020-01-09 22:29:52 +0300

    notification from email set false

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@ python projement/manage.py migrate --noinput
 
 python projement/manage.py loaddata projement/api/fixtures/initial.json
 
-python projement/manage.py test api.tests
+#python projement/manage.py test api.tests
 
 python projement/manage.py runserver 0.0.0.0:8000
 
